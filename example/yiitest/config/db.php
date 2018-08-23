463b42e0b4e3dc9e067e15f820ca1eadf5d9d57c
reinhardtken <reinhardtken@hotmail.com>
2018-08-23 22:30:21 +0800

    Signed-off-by: reinhardtken <reinhardtken@hotmail.com>

--- example/yiitest/config/db.php
+++ example/yiitest/config/db.php
@@ -1,23 +1,23 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
-
-
 //return [
 //    'class' => 'yii\db\Connection',
-//    'dsn' => 'mysql:host=localhost;dbname=php7_test',
+//    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
 //    'username' => 'root',
-//    'password' => '123456',
+//    'password' => '',
 //    'charset' => 'utf8',
+//
+//    // Schema cache options (for production environment)
+//    //'enableSchemaCache' => true,
+//    //'schemaCacheDuration' => 60,
+//    //'schemaCache' => 'cache',
 //];
+
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=php7_test',
+    'username' => 'root',
+    'password' => '123456',
+    'charset' => 'utf8',
+];
